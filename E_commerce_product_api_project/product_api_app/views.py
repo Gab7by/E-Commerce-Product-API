169cd39fa901bf296a32819521414fec81ab457e
Nana Kwame <owusugabriel803@gmail.com>
2025-12-07 07:56:31 +0000

    Created views for Users and Products with POST and GET api view

--- E_commerce_product_api_project/product_api_app/views.py
+++ E_commerce_product_api_project/product_api_app/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import UserSerializer, ProductSerializer
 from .models import User, Product
 
-@api_view()
+@api_view(['POST'])
 def api_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    serializer = UserSerializer(data=request.data)
+    if serializer.Is_valid():
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view()
+@api_view(['GET'])
 def api_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
