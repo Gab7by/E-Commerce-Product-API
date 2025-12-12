1e5937dd179fe7d8a9aebd1380d9f4de5f871ddd
Nana Kwame <owusugabriel803@gmail.com>
2025-12-12 22:08:01 +0000

    Added search and filter options

--- E_commerce_product_api_project/E_commerce_product_api_project/settings.py
+++ E_commerce_product_api_project/E_commerce_product_api_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'product_api_app.apps.ProductApiAppConfig'
+    'product_api_app.apps.ProductApiAppConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- E_commerce_product_api_project/product_api_app/views.py
+++ E_commerce_product_api_project/product_api_app/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
 from .serializers import UserSerializer, ProductSerializer
 from .models import User, Product
-from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilter
+from rest_framework.filters import SearchFilter
 
 
 class ApiUsers(CreateAPIView):
@@ -15,6 +14,9 @@ class ApiUsers(CreateAPIView):
 class ApiProducts(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = ProductFilter
+    search_fields = ['name', 'category']
      
 class ApiProduct(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
