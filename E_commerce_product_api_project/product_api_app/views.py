b61ad5fc9f7f251f60d0419106c7a1154a181d6d
Nana Kwame <owusugabriel803@gmail.com>
2026-01-03 13:27:50 +0000

    Final Final commit

--- E_commerce_product_api_project/product_api_app/views.py
+++ E_commerce_product_api_project/product_api_app/views.py
@@ -16,11 +16,11 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            token, _ = Token.objects.get_or_create(user=user)
+            serializer.save()
+           
             return Response({
                 'message': 'User registered succesfully',
-                'token' : token.key
+                
             }, status=status.HTTP_201_CREATED)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
